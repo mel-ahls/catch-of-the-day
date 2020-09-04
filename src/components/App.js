c2698629a33097e25770679f307bcf14ce6d334b
Melissa Ahlstrom <melissa.ahlstrom@hotmail.com>
2020-09-04 14:24:10 -0400

    last update - fish component

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,56 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      fishes: {},
+      order: {}
+      // another way instead of using constructor
+      // state = {
+      //   fishes: {},
+      //   order: {}
+      // };
+    }
+  }
+
+  addFish = fish => {
+    // take a copy of the existing state
+    const fishes = {...this.state.fishes};
+    // add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // set the new fishes object to state
+    this.setState({
+      fishes
+    })
+  }
+
+  loadSampleFishes = () => {
+    this.setState({ 
+      fishes: sampleFishes
+    })
+  }
+
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Wes is cool"/>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory 
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     )
   }
